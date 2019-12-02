4e8d2c48fcfc09c2060e81d04615ad745d311b0e
Selt0 <chewedgum12@gmail.com>
2019-12-02 14:57:55 -0600

    Improve emailParts function. For loop unecessary

--- palindrome.js
+++ palindrome.js
@@ -16,16 +16,18 @@ function palindrome(string) {
 //separates user email and domain
 function emailParts(string) {
   //create an array including the characters
- let email = Array.from(string);
-  //loop through array until '@' is found.
-  for (let i = 0; i < email.length; i++) {
-    if (email[i] === '@') {
-      //slice starting from '@' location.
-     let domain =  email.slice(i + 1).join("");
-     let username = email.slice(0, i).join("");
-     console.log(`Your user name is ${username}. The domain for the email address is ${domain}.`)
-    }
-  }
+//  let email = Array.from(string);
+//   //loop through array until '@' is found.
+//   for (let i = 0; i < email.length; i++) {
+//     if (email[i] === '@') {
+//       //slice starting from '@' location.
+//      let domain =  email.slice(i + 1).join("");
+//      let username = email.slice(0, i).join("");
+//      console.log(`Your user name is ${username}. The domain for the email address is ${domain}.`)
+//     }
+//   }
   //print out  username and domain.
-  return string.toLowerCase().split("@");
+  let email = string.toLowerCase().split("@");
+  console.log(`your username is ${email[0]} and the domain is ${email[1]}.`);
+  return email;
 }
